d744f5f5a42dbbd2d9d56d4e7da709f88ef12fa0
chhak0503 <chhak0503@gmail.com>
2023-01-11 16:11:58 +0900

    2023/01/11 수업예제

---
+++ Ch07/src/main/java/kr/co/ch07/repository/User1Repo.java
@@ -0,0 +1,12 @@
+package kr.co.ch07.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.query.JpaEntityMetadata;
+import org.springframework.stereotype.Repository;
+
+import kr.co.ch07.vo.User1VO;
+
+@Repository
+public interface User1Repo extends JpaRepository<User1VO, String> {
+
+}
